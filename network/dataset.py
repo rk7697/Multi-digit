6652868baebb793c4523c41848122523c18a3561
rk7697 <rileyk7697@gmail.com>
2024-10-02 17:51:03 -0700

    add code

--- network/dataset.py
+++ network/dataset.py
@@ -3,37 +3,37 @@ from torchvision import (
     datasets,
     transforms
 )
+from torchvision.transforms.functional import pad
+from cv2 import Canny
+
 
 #Rotate image by a randomly selected angle between MIN_ANGLE and MAX_ANGLE degrees
 def rotate_img(image):
     MIN_ANGLE = -15
     MAX_ANGLE = 15
     angle=random.uniform(MIN_ANGLE,MAX_ANGLE)
-    return transforms.functional.Rotate(img=image,angle=angle)
+    return transforms.functional.rotate(img=image,angle=angle)
 
 #Pad image with specified new size 
 #and shift image center to a random location
 def pad_shift(image,new_size_width,new_size_height):
-    image_height=image.size(0)
-    image_width=image.size(1)
+    image_height=image.shape[-2]
+    image_width=image.shape[-1]
 
     #Move the center to a random place in the valid range
-    half_image_height = image_height//2,
-    half_image_width = image_width//2,
+    half_image_height = image_height//2
+    half_image_width = image_width//2
 
-    new_center_y = random.uniform(half_image_height, new_size_height - half_image_height)
-    new_center_x = random.uniform(half_image_width, new_size_width - half_image_width)
+    new_center_y = int(random.uniform(half_image_height, new_size_height - half_image_height))
+    new_center_x = int(random.uniform(half_image_width, new_size_width - half_image_width))
 
     #Define padding (left, right, top bottom)
     padding_left = new_center_x - half_image_width
     padding_right = new_size_width - (new_center_x + half_image_width)
     padding_bottom = new_center_y - half_image_height
     padding_top = new_size_height - (new_center_y + half_image_height)
-    image = transform.pad(image, (padding_left,padding_right,padding_top,padding_bottom))
+    image = pad(image, (padding_left, padding_right, padding_top, padding_bottom))
     return image
-
-
-    
 
 #Set new_size to be 500,500 for MNIST image
 new_size_width=500
@@ -42,10 +42,9 @@ new_size_height=500
 transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Lambda(lambda x: 1-x), #Invert image to black with white background
-    rotate_img(), #Rotate image by a random angle in the specified interval
+    rotate_img, #Rotate image by a random angle in the specified interval
     transforms.Lambda(lambda x: pad_shift(x, new_size_width, new_size_height)), #Shift center of image to random location in the specified new image dimensions   
 ])
 
 train_dataset = datasets.MNIST("./data",train=True,download=False,transform=transform)
 test_dataset = datasets.MNIST("./data",train=False,download=False,transform=transform)
-exit()
--- network/network.py
+++ network/network.py
@@ -7,86 +7,63 @@ import matplotlib.pyplot as plt
 import numpy as np
 from torch.nn import init
 import os
-# dir_path = os.path.dirname(os.path.realpath(__file__))
-from .dataset import f
-f()
-exit()
-
-
-exit()
-from dir_path.dataset import f
-exit()
-print(dir_path)
-exit()
-from dataset import MNISTdata
-
-
-
-class net(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.activation=nn.ReLU()
-    ###Forward pass
-    def forward(self,x):
-        if self.linear:
-            linearlayers=[layer for layer in list(self.children()) if (isinstance(layer,(nn.Linear)))]
-            x=torch.flatten(x,start_dim=1)
-            layerindex=0
-            for layer in linearlayers:
-                layerindex+=1
-                x=layer(x)
-                if(layer != linearlayers[-1]):
-                    x=self.activation(x)
-            return x
-        else:
-            cnnsandmaxpoollayers=[layer for layer in list(self.children()) if (isinstance(layer,(nn.Conv2d)) or isinstance(layer,(nn.MaxPool2d)))]
-            linearlayers=[layer for layer in list(self.children()) if (isinstance(layer,(nn.Linear)))]
-            layerindex=0
-            for layer in (cnnsandmaxpoollayers):
-                if(isinstance(layer,nn.Conv2d)):
-                    layerindex+=1
-                    x=layer(x)
-                    x=self.activation(x)
-            x=torch.flatten(x,start_dim=1)
-            for layer in linearlayers:
-                layerindex+=1
-                x=layer(x)
-                self.recentoutputs.append(x)
-                if(layer != linearlayers[-1]):
-                    x=self.activation(x)
-            return x     
+from .dataset import train_dataset
+dataloader=DataLoader(train_dataset,batch_size=1,shuffle=True)
+for labels, imgs in dataloader:
+    print(1)
+    exit()
+# class net(nn.Module):
+#     def __init__(self):
+#         super().__init__()
+#         self.activation=nn.ReLU()
+#     def forward(self,x):
+#             cnnsandmaxpoollayers=[layer for layer in list(self.children()) if (isinstance(layer,(nn.Conv2d)) or isinstance(layer,(nn.MaxPool2d)))]
+#             linearlayers=[layer for layer in list(self.children()) if (isinstance(layer,(nn.Linear)))]
+#             layerindex=0
+#             for layer in (cnnsandmaxpoollayers):
+#                 if(isinstance(layer,nn.Conv2d)):
+#                     layerindex+=1
+#                     x=layer(x)
+#                     x=self.activation(x)
+#             x=torch.flatten(x,start_dim=1)
+#             for layer in linearlayers:
+#                 layerindex+=1
+#                 x=layer(x)
+#                 self.recentoutputs.append(x)
+#                 if(layer != linearlayers[-1]):
+#                     x=self.activation(x)
+#             return x     
 ###
-dataset="MNIST_new"
-rootdir=os.path.join("/Users/rileyknutson/desktop/",dataset)
-optimizer=torch.optim.Adam
+
+# optimizer=torch.optim.Adam
 
 
 
-trainData=MNISTdata(rootdir,"train")
+# trainData=MNISTdata(rootdir,"train")
 
-torch.optim.Adam(net.parameters(),lr=.001,weight_decay=.0000001)
+# torch.optim.Adam(net.parameters(),lr=.001,weight_decay=.0000001)
 
-def train(net,optimizer,epochs):
-    lossfunc=nn.CrossEntropyLoss()
-    errors=[]
-    epochcount=[]
-    for epoch in range(epochs):
-        traindataloader=DataLoader(trainData,batch_size=32,shuffle=True)
-        runningerror=0.0
-        i=0
-        for labels, imgs in traindataloader:
-            optim.zero_grad()
-            logits=net.forward(imgs)
-            loss=lossfunc(logits,labels)
-            loss.backward()
-            optim.step()
-            runningerror+=loss
-            if(i>0 and i%(10)==0):
-                avgerror=runningerror/i
-                errors.append(avgerror)
-                percent=epoch+i/len(traindataloader)
-                epochcount.append(percent)
-                if(i%100==0):
-                    print("error:{:<10} epoch:{:<2} percent:{:<7.2f}%".format(avgerror, epoch, percent))
-            i+=1
-    return epochcount,errors,  
+# def train(net,optimizer,epochs):
+#     lossfunc=nn.CrossEntropyLoss()
+#     errors=[]
+#     epochcount=[]
+#     for epoch in range(epochs):
+#         traindataloader=DataLoader(trainData,batch_size=32,shuffle=True)
+#         runningerror=0.0
+#         i=0
+#         for labels, imgs in traindataloader:
+#             optim.zero_grad()
+#             logits=net.forward(imgs)
+#             loss=lossfunc(logits,labels)
+#             loss.backward()
+#             optim.step()
+#             runningerror+=loss
+#             if(i>0 and i%(10)==0):
+#                 avgerror=runningerror/i
+#                 errors.append(avgerror)
+#                 percent=epoch+i/len(traindataloader)
+#                 epochcount.append(percent)
+#                 if(i%100==0):
+#                     print("error:{:<10} epoch:{:<2} percent:{:<7.2f}%".format(avgerror, epoch, percent))
+#             i+=1
+#     return epochcount,errors,  
