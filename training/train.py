b3727175a45aefd8a426f65c66d2765b0582a6ac
rk7697 <rileyk7697@gmail.com>
2024-10-03 03:14:04 -0700

    add

--- training/train.py
+++ training/train.py
@@ -35,19 +35,19 @@ def train(network, num_epochs, train_dataloader):
 
             optimizer.step()
 
-            error+=loss
+            error+=loss.item()
 
             if((batch_index +1) % PRINT_INTERVAL == 0):
                 avg_error = error / batch_index
                 error_log.append(avg_error)
 
-                epoch_progress = batch_index / (num_batches * num_epochs)
+                epoch_progress = (epoch + batch_index / num_batches) / num_epochs # Calculate progress of the current epoch based on batch_index
                 print(f"error: {avg_error:.5f} percent: {epoch_progress:.2f}")
 
 def plot_error(error_log, num_epochs):
     num_samples = len(error_log)
 
-    log_intervals = [i * num_epochs/num_samples for i in range(num_samples)] #Calculate intervals based on num_samples and num_epochs
+    log_intervals = [i * num_epochs/num_samples for i in range(num_samples)] # Calculate intervals based on num_samples and num_epochs
     
     plt.plot(log_intervals, error_log)
     plt.xlabel("Epoch")
