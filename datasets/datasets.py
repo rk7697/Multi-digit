bf1aeba0ed646deb9a66c72de5ecc843bf00f83b
rk7697 <rileyk7697@gmail.com>
2024-10-04 14:27:20 -0700

    Add function resize_image, debug pad_shift function Add resize_image to randomly resize images for training Debug pad_shift by accounting for odd image dimensions

--- datasets/datasets.py
+++ datasets/datasets.py
@@ -1,3 +1,4 @@
+import math
 import random
 import numpy as np
 import torch
@@ -8,6 +9,7 @@ from torchvision import (
 )
 from torchvision.transforms.functional import pad
 import torchvision.transforms as transforms
+
 
 # Apply canny edge detection to image
 def canny_edge_detection(image):
@@ -23,8 +25,19 @@ def canny_edge_detection(image):
 def rotate_img(image):
     MIN_ANGLE = -15
     MAX_ANGLE = 15
-    angle=random.uniform(MIN_ANGLE,MAX_ANGLE)
+    angle = random.uniform(MIN_ANGLE, MAX_ANGLE)
     return transforms.functional.rotate(img = image, angle = angle, fill=0)
+
+# Resize image to randomly selected height and width between MIN_DIMENSION and MAX_DIMENSION pixels
+def resize_img(image):
+    MIN_DIMENSION = 28
+    MAX_DIMENSION = 100 # Set MAX_DIMENSION to dimension very close to new_size dimensions for use case
+
+    # Resize image to random dimensions in the valid range
+    new_height = random.randint(MIN_DIMENSION, MAX_DIMENSION + 1) 
+    new_width = random.randint(MIN_DIMENSION, MAX_DIMENSION + 1)
+    resize_transform = transforms.Resize((new_height, new_width)) # Define resize transform for image
+    return resize_transform(image)
 
 # Pad image with specified new size 
 # and shift image center to a random location
@@ -32,32 +45,36 @@ def pad_shift(image,new_size_width,new_size_height):
     image_height=image.shape[-2]
     image_width=image.shape[-1]
 
-    # Move the center to a random place in the valid range
-    half_image_height = image_height//2
-    half_image_width = image_width//2
+    # Calculate halved image dimensions
+    half_image_height = image_height/2
+    half_image_width = image_width/2
 
-    new_center_y = int(random.uniform(half_image_height, new_size_height - half_image_height))
-    new_center_img = int(random.uniform(half_image_width, new_size_width - half_image_width))
+    # Generate random integer center in random place in the valid range
+    # and subtract the fractional part of half dimension to calculate a valid center
+    new_center_y = int(random.uniform(half_image_height, new_size_height - half_image_height)) - math.modf(half_image_height)[0]
+    new_center_x = int(random.uniform(half_image_width, new_size_width - half_image_width)) - math.modf(half_image_width)[0]
 
     # Define padding (left, right, top, bottom)
-    padding_left = new_center_img - half_image_width
-    padding_right = new_size_width - (new_center_img + half_image_width)
-    padding_bottom = new_center_y - half_image_height
-    padding_top = new_size_height - (new_center_y + half_image_height)
-
+    padding_left = int(new_center_x - half_image_width)
+    padding_right = int(new_size_width - (new_center_x + half_image_width))
+    padding_bottom = int(new_center_y - half_image_height)
+    padding_top = int(new_size_height - (new_center_y + half_image_height))
+    
     PAD_FILL_VALUE=0 # Set the pad fill value to black
     image = pad(image, (padding_left, padding_top, padding_right, padding_bottom), fill = PAD_FILL_VALUE)
     return image
 
-# Set new_size to be 50,50 for MNIST image
-new_size_width=50
-new_size_height=50
+# Set new_size to be 100, 100 for MNIST image
+# For now, use small input size for use case
+new_size_width = 112
+new_size_height = 112
 
 transform = transforms.Compose([
     # transforms.Lambda(lambda img: canny_edge_detection(img)),
     transforms.ToTensor(), # Convert to tensor
     transforms.Normalize(mean=0, std=1), # Normalizer the tensor
-    rotate_img, # Rotate image by a random angle in the specified interval
+    rotate_img, # Rotate image by a random angle in the specified range
+    resize_img, # Resize image to random dimensions in the specified range
     transforms.Lambda(lambda img: pad_shift(img, new_size_width, new_size_height)), # Shift center of image to random location in the specified new image dimensions   
     transforms.Lambda(lambda img: img.to(torch.float32))
 ])
--- network/network.py
+++ network/network.py
@@ -5,15 +5,19 @@ class multi_digit(nn.Module):
         super().__init__()
         self.activation=nn.ReLU()
 
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=20, kernel_size=3)
-        self.conv2 = nn.Conv2d(in_channels=20, out_channels=30, kernel_size=3)
-        self.conv3 = nn.Conv2d(in_channels=30, out_channels=40, kernel_size=3)
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=20, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=20, out_channels=30, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=30, out_channels=40, kernel_size=3, padding=1)
+        self.conv4 = nn.Conv2d(in_channels=40, out_channels=50, kernel_size=3, padding=1)
+        self.conv5 = nn.Conv2d(in_channels=50, out_channels=50, kernel_size=3, padding=1)
 
         self.pool1 = nn.MaxPool2d(kernel_size=2)
         self.pool2 = nn.MaxPool2d(kernel_size=2)
         self.pool3 = nn.MaxPool2d(kernel_size=2)
+        self.pool4 = nn.MaxPool2d(kernel_size=2)
+        self.pool5 = nn.MaxPool2d(kernel_size=2)
 
-        self.linear1 = nn.Linear(4*4*40,50)
+        self.linear1 = nn.Linear(3*3*50,50)
         self.linear2 = nn.Linear(50,10)
 
     def forward(self,x):
@@ -26,6 +30,12 @@ class multi_digit(nn.Module):
             x = self.conv3(x)
             x = self.activation(x)
             x = self.pool3(x)
+            x = self.conv4(x)
+            x = self.activation(x)
+            x = self.pool4(x)
+            x = self.conv5(x)
+            x = self.activation(x)
+            x = self.pool5(x)
 
             # Flatten tensor by
             # reshaping to (batch_size , -1)
